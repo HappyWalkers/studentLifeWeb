2f6d5ee106f8e57e8b43be7754f79057893d920d
Wang <yunpengw@kite.upenn.edu>
2022-08-28 15:32:15 -0400

    create index page for course review app and collect static files

--- courseReview/views.py
+++ courseReview/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import college, department, major, course, professor, extendUser, review
+
 # Create your views here.
+def index(request):
+    """view function for index page of course review"""
+    num_users = extendUser.objects.all().count()
+
+    context = {
+        'num_users' : num_users
+    }
+
+    return render(request, 'index.html', context=context)
