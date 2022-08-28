3fe434089fcc6d76aac95810945a684c79e64c0d
HappyWalkers <763435962@qq.com>
2022-08-28 11:05:04 -0400

    add __str__() for classes in courseReview.models

--- courseReview/models.py
+++ courseReview/models.py
@@ -9,6 +9,10 @@ class college(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, help_text="enter the name of the college")
     introduction = models.TextField(max_length=1000, help_text="enter a brief introduction of the college")
+    
+    def __str__(self):
+        return self.name
+
 
 class department(models.Model):
     """model representing different departments"""
@@ -16,6 +20,9 @@ class department(models.Model):
     name = models.CharField(max_length=200, help_text="enter the name of the department")
     introduction = models.TextField(max_length=1000, help_text="enter a brief introduction of the department")
     college = models.ForeignKey(college, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
 
 class major(models.Model):
     """model representing different majors"""
@@ -24,6 +31,9 @@ class major(models.Model):
     introduction = models.TextField(max_length=1000, help_text="enter a brief introduction of the major")
     college = models.ForeignKey(college, on_delete=models.CASCADE)
     department = models.ForeignKey(department, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
 
 class course(models.Model):
     """model representing different courses"""
@@ -34,6 +44,9 @@ class course(models.Model):
     department = models.ForeignKey(department, on_delete=models.CASCADE)
     professor = models.ManyToManyField("professor", help_text="select a professor for the course")
     rating = models.IntegerField(blank=True, validators=[MaxValueValidator(100), MinValueValidator(0)], help_text="average score of the course rating")
+    
+    def __str__(self):
+        return self.name
 
 class professor(models.Model):
     """model representing different professors"""
@@ -44,6 +57,9 @@ class professor(models.Model):
     department = models.ForeignKey(department, on_delete=models.CASCADE)
     # courses = models.ManyToManyField(course, help_text="courses taught by the professor") # professor's courses could be accessed by course.professor
     rating = models.IntegerField(blank=True, validators=[MaxValueValidator(100), MinValueValidator(0)], help_text="average score of the courses ratings which taught by the professor")
+    
+    def __str__(self):
+        return self.name
 
 class extendUser(models.Model):
     """model extend the profile from login app"""
@@ -51,6 +67,9 @@ class extendUser(models.Model):
     college = models.OneToOneField(college, on_delete=models.CASCADE)
     major = models.OneToOneField(major, on_delete=models.CASCADE)
     courses = models.ManyToManyField(course, blank=True, help_text="courses taken by the user")
+    
+    def __str__(self):
+        return self.basicUser.user.first_name.title
 
 class review(models.Model):
     """model representing the review posted by a extendUser to a course"""
@@ -62,5 +81,8 @@ class review(models.Model):
     rating = models.IntegerField(blank=False, validators=[MaxValueValidator(100), MinValueValidator(0)], help_text="rate this course")
     score = models.IntegerField(blank=True, validators=[MaxValueValidator(100), MinValueValidator(0)], help_text="enter your score of the course")
     qualityRating = models.IntegerField(blank=False, validators=[MaxValueValidator(100), MinValueValidator(0)], help_text="rate the qulaity of the course")
+    
+    def __str__(self):
+        return self.content
 
     
